c8f608c86c7ba4307167a1d71815eee3a9250da5
kadamswapnil4259@gmail.com <kadamswapnil4259@gmail.com>
2026-06-23 20:45:31 +0530

    change images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -9,10 +9,19 @@ import MexicanBurger from './mexican_burger.png';
 import PaneerBurger from './paneer_burger.png';
 import BBQChickenBurger from './bbq_burger.png';
 import MargheritaPizza from './margherita_pizza.png';
+import FarmhousePizza from './farmhouse_pizza.png';
+import PaneerPizza from './paneer_tandori_pizza.png';
+import SpicyPizza from './spicy_pizza.png';
+import PepporiniPizza from './pepporani_pizza.png';
+import FieryChickenPizza from './fiery_chicken_pizza.png';
 
 import ChickenPopocorn from './chciken_popcorn.png';
 
 
+import MasalaMaggi from './masala_maggie.png';
+import PeriMaggie from './peri_maggie.png';
+
+import GarlicBread from './garlic_bread.png';
 
 
 
@@ -30,6 +39,14 @@ export const image = (name) => {
     "bbq-burger": BBQChickenBurger,
     "margherita-pizza": MargheritaPizza,
     "chicken-popcorn": ChickenPopocorn,
+    "farmhouse-pizza" : FarmhousePizza,
+    "paneer-tandori-pizza" : PaneerPizza,
+    "spicy-pizza" : SpicyPizza,
+    "pepporini-pizza" : PepporiniPizza,
+    "fiery-chicken-pizza" : FieryChickenPizza,
+    "masala-maggi" : MasalaMaggi,
+    "peri-maggi" : PeriMaggie,
+    "garlic-bread" : GarlicBread,
   };
   return images[name];
 }
